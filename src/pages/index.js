f5fc2cd89b7d70d64e0e9cbf36db44e7a3a77ce0
Adhithya Ramakumar <adi@Adhithyas-iMac.attlocal.net>
2018-08-09 23:26:13 -0700

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>I am learning ReactJS</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">make some component changes</Link>
+  </div>
+)
+
+export default IndexPage
